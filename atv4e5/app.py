c2a54102a41e0df3f0dbfae021ac8e46a4579948
andermrs <andranderlaine@gmail.com>
2026-05-22 11:10:40 -0300

    Add files via upload

---
+++ atv4e5/app.py
@@ -0,0 +1,28 @@
+
+from flask import Flask, render_template
+import jinja2
+
+app = Flask(__name__)
+
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/layout")
+def layout():
+    return render_template("layout.html")
+
+
+@app.route("/page1")
+def page1():
+    return render_template("page1.html")
+
+
+@app.route("/page2")
+def page2():
+    return render_template("page2.html")     
+
+if __name__ == "__main__":
+    app.run(debug=True)   
